15becf5fc40963b8a2725016fe4419fe7ceb2175
djmcnutt <56748710+djmcnutt@users.noreply.github.com>
2019-12-13 23:53:24 -0500

    Add files via upload

--- server.R
+++ server.R
@@ -1,35 +1,97 @@
 library(shiny)
 library(dplyr)
 library(ggplot2)
+library(readxl)
+library(knitr)
+library(MuMIn)
+
+goldData <- read_excel("GoldenState2015_16.xlsx")
 
 shinyServer(function(input, output, session) {
+  
+  getData3 <- reactive({
+    newData3 <- goldData
+  })
+  
+  getData2 <- reactive({
+    newData2 <- goldData %>% filter(player == input$name)
+  })
   
   getData <- reactive({
     newData <- msleep %>% filter(vore == input$vore)
   })
   
-  #create plot
-  output$sleepPlot <- renderPlot({
+  selectedData <- reactive({
+    goldData[, c(input$xcol, input$ycol)]
+  })
+  
+  clusters <- reactive({
+    kmeans(selectedData(), input$clusters)
+  })
+  
+  output$plot1 <- renderPlot({
+    palette(c("#E41A1C", "#377EB8", "#4DAF4A", "#984EA3",
+              "#FF7F00", "#FFFF33", "#A65628", "#F781BF", "#999999"))
+    
+    par(mar = c(5.1, 4.1, 0, 1))
+    plot(selectedData(),
+         col = clusters()$cluster,
+         pch = 20, cex = 3)
+    points(clusters()$centers, pch = 4, cex = 4, lwd = 4)
+  })
+  
+  model <- reactive({
+    
+    compareFitStats <- function(fit1, fit2){
+      
+      require(MuMIn)
+      fitStats <- data.frame(fitStat = c("Adj R Square", "AIC", "AICc", "BIC"),
+                             col1 = round(c(summary(fit1)$adj.r.squared, AIC(fit1), 
+                                            MuMIn::AICc(fit1), BIC(fit1)), 3),
+                             col2 = round(c(summary(fit2)$adj.r.squared, AIC(fit2), 
+                                            MuMIn::AICc(fit2), BIC(fit2)), 3))
+      #put names on returned df
+      calls <- as.list(match.call())
+      calls[[1]] <- NULL
+      names(fitStats)[2:3] <- unlist(calls)
+      fitStats
+    }
+    
+    fgFit1 <- lm(fg_perc ~ avg_dist, data = selectedData())
+    fgFit2 <- lm(input$ycol ~ input$xcol)
+    
+    compareFitStats(fgFit1,fgFit2)
+  })
+  
+  prediction <- reactive({
+    
+    fgFit2 <- lm(input$ycol ~ input$xcol, data = selectedData())
+    
+    predict(fgFit2,newData=selectedData())
+  })
+
+  #create text info
+  output$info2 <- renderText({
     #get filtered data
-    newData <- getData()
+
+    paste("The variables you chose were ", input$xcol, " and ", input$ycol, ". Here is the model:", model(),  "Here's your prediction: ", prediction(), sep = " ")
+    
+  })
+  
+  #create plot
+  output$teamPlot <- renderPlot({
+    #get filtered data
+    newData <- getData3()
     
     #create plot
-    g <- ggplot(newData, aes(x = bodywt, y = sleep_total))
+    g <- ggplot(newData, aes(x = player, y = fg_3pt))
     
-    if(input$conservation){
-      
-      if (input$rem) {
+    if(input$team) {
         
-        g + geom_point(size = input$size, aes(col = conservation, alpha=0.5))
-        
-      } else {
-        
-      g + geom_point(size = input$size, aes(col = conservation))
-        
-      }
+        g + geom_point(size = input$size, aes(col = team,size=qsec))
       
     } else {
-      g + geom_point(size = input$size)
+        g + geom_point(size = input$size,aes(size=qsec))
     }
   })
   
@@ -37,23 +99,14 @@ shinyServer(function(input, output, session) {
   #create text info
   output$info <- renderText({
     #get filtered data
-    newData <- getData()
+    newData <- getData2()
     
-    paste("The average body weight for order", input$vore, "is", round(mean(newData$bodywt, na.rm = TRUE), 2), "and the average total sleep time is", round(mean(newData$sleep_total, na.rm = TRUE), 2), sep = " ")
+    paste("The field goal percentage for shots made from the 3 point range for", input$name, "is", newData$fg_3pt[newData$player==input$name], "and the field goal percentage for shots made from the corner 3 point range is", newData$fg_3pt_corner[newData$player==input$name],  sep = " ")
   })
   
   #create output of observations    
   output$table <- renderTable({
-    getData()
-  })
-  
-  observe({
-  
-  if (input$rem) { updateSliderInput(session, "size", min=3) 
-    
-  } else { updateSliderInput(session, "size", min=1) 
-  }
-    
+    data.frame(getData3())
   })
   
   output$infonew <- renderUI({
--- ui.R
+++ ui.R
@@ -1,4 +1,7 @@
 library(ggplot2)
+library(readxl)
+
+goldData <- read_excel("GoldenState2015_16.xlsx")
 
 shinyUI(fluidPage(
   
@@ -8,20 +11,41 @@ shinyUI(fluidPage(
   # Sidebar with options for the data set
   sidebarLayout(
     sidebarPanel(
-      h3("Select the mammal's biological order:"),
-      selectizeInput("vore", "Vore", selected = "omni", choices = levels(as.factor(msleep$vore))),
+      h5("Select a Player for Quick Player Details:"),
+      selectizeInput("name", "Player Name", selected = "Stephen Curry", choices = levels(as.factor(goldData$player))),
       br(),
-      sliderInput("size", "Size of Points on Graph",
+      h5("Select Size of Points or Check Box for Explore Tab"),
+      sliderInput("size", "Size of Points on Graph (Used for ...)",
                   min = 1, max = 10, value = 5, step = 1),
-      checkboxInput("conservation", h4("Color Code Conservation Status", style = "color:red;")),
-      conditionalPanel(condition="input.conservation",checkboxInput("rem",h4("REM Cycle Opacity")))
+      checkboxInput("team", h4("Color by Team", style = "color:black;")),
+      h5("Select Variables and Cluster Count for Cluster Tab"),
+      selectInput('xcol', 'X Variable', names(goldData),selected="age"),
+      selectInput('ycol', 'Y Variable', names(goldData),
+                  selected="fg_perc"),
+      numericInput('clusters', 'Cluster count', 3,
+                   min = 1, max = 9)  
+      
     ),
     
     # Show outputs
     mainPanel(
-      plotOutput("sleepPlot"),
-      textOutput("info"),
-      tableOutput("table")
+      tabsetPanel(type="tabs",
+         tabPanel("Background",
+                   h4("About the Data"),
+                   h5("The 2015-16 Golden State Warriors were one of if not THE greatest team of all time in NBA Basketball history. This season was the 70th season of the franchise and they entered the season as the defending champs from the previous year in which they went 67-15. This season they would do something even more incredible though posting an impressive and record-breaking 73-9. What's even more impressive was that this team didn't really have a major front man like Michael Jordan. Instead the entire team was working together and was known for its 3pt shooting even going as far as to shoot the 3pt shot BEYOND the 3 point line. I love this story because I love it when people overcome aribtrary boundaries. There is also data here for the Cleveland Cavaliers team that barely beat them in the NBA Finals of the same season."),
+                  h4("Abilities of the App"),
+                  h5("This app allows the user to select variables and other user specified information for clustering and for modeling as well as display stats for individually selected players and make changes to the plotting output."),
+                  h4("Quick Player Details"),
+                  textOutput("info")
+                   ),
+         tabPanel("Explore",plotOutput("teamPlot")),
+         tabPanel("Cluster",plotOutput("plot1")),
+         tabPanel("Modeling",
+                  h4("Which Variable do you think best predicts 3 point Percentage?"),
+                  h4("Choose from the x and y variables"),                
+                  textOutput("info2")),   
+         tabPanel("Table",tableOutput("table"))
+      )
     )
   )
 ))
